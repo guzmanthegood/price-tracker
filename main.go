6bd9de97cfa0bb69ee9d8ecc3bcc6f2e4b4fbc58
cguzman <cguzman@xmltravelgate.com>
2018-05-18 12:25:27 +0200

    Catch idCache and launch availability to ajax call

--- main.go
+++ main.go
@@ -26,13 +26,3 @@ func main() {
 
 	logger.Info("===== price-Tracker application End   ======")
 }
-
-/*
-
-input := "2017-08-31"
-layout := "2006-01-02"
-t, _ := time.Parse(layout, input)
-fmt.Println(t)                       // 2017-08-31 00:00:00 +0000 UTC
-fmt.Println(t.Format("02-Jan-2006")) // 31-Aug-2017
-
-*/
--- scraper/tools.go
+++ scraper/tools.go
@@ -1,7 +1,7 @@
 package scraper
 
 import (
-	"fmt"
+	"net/url"
 	"time"
 )
 
@@ -17,12 +17,45 @@ type Price struct {
 }
 
 // getAvailabilityURL Logitravel availability creation
-func getAvailabilityURL(o, d string, dd time.Time) string {
-	return fmt.Sprintf(`https://www.logitravel.com/flightssaleprocess/availability?`+
-		`spc=&ciaAerea=&ciasCon=&vueloslowcost=on&clase=0&descuentos=0`+
-		`&hidOrigenSV=%v&hidDestinoSV=%v&searchTypeSV=%v`+
-		`&fechaIdaSV=%v&fechaVueltaSV=%v&adultos=%v&ninos=%v&bebes=%v`,
-		o, d, "OneWay", dd.Format("02/01/2006"), "", 1, 0, 0)
+func getAvailabilityURL(o, d string, d1 time.Time) string {
+	str, _ := url.Parse("https://www.logitravel.com")
+	str.Path += "/flightssaleprocess/availability"
+	parameters := url.Values{}
+	parameters.Add("vueloslowcost", "on")
+	parameters.Add("clase", "0")
+	parameters.Add("descuentos", "0")
+	parameters.Add("hidOrigenSV", o)
+	parameters.Add("hidDestinoSV", d)
+	parameters.Add("searchTypeSV", "OneWay")
+	parameters.Add("fechaIdaSV", d1.Format("02/01/2006"))
+	parameters.Add("fechaVueltaSV", "")
+	parameters.Add("adultos", "1")
+	parameters.Add("ninos", "")
+	parameters.Add("bebes", "")
+	str.RawQuery = parameters.Encode()
+	return str.String()
+}
+
+// getAvailabilityURL Logitravel availability creation
+func getFilteredAvailabilityURL(o, d string, d1 time.Time, idCache string) string {
+	var hashItineraries = o + "." + d + "." + d1.Format("2006-01-02")
+
+	str, _ := url.Parse("https://www.logitravel.com")
+	str.Path += "/flightssaleprocess/Filter/GetFilteredAvailability"
+	parameters := url.Values{}
+	parameters.Add("idCache", idCache)
+	parameters.Add("stopOver", "0,1")
+	parameters.Add("companies", "")
+	parameters.Add("allCompaniesSelected", "true")
+	parameters.Add("airports", "")
+	parameters.Add("sortCriteria", "0")
+	parameters.Add("discount", "0")
+	parameters.Add("currentPage", "2")
+	parameters.Add("hours", "0,23;0,23;")
+	parameters.Add("hashItineraries", hashItineraries)
+	parameters.Add("includeSharedCodes", "true")
+	str.RawQuery = parameters.Encode()
+	return str.String()
 }
 
 // diffDays get diff days from two dates
